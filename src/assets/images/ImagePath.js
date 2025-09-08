88429d082c084e8a414fb445d52090fdf16831be
hamid raza <56587513+hamidraza2788@users.noreply.github.com>
2025-09-08 17:31:36 +0500

    project setup

---
+++ src/assets/images/ImagePath.js
@@ -0,0 +1,19 @@
+export default {
+    Profile: require("./Profile.png"),
+    SlideImage1: require("./slider1.png"),
+    SlideImage2: require("./slider2.png"),
+    Product1: require("./product1.png"),
+    Product2: require("./product2.png"),
+    Product3: require("./product3.png"),
+    Product4: require("./product4.png"),
+    Product5: require("./product5.png"),
+    Product6: require("./product6.png"),
+    Product7: require("./product7.png"),
+    Product8: require("./product8.png"),
+    Product8: require("./product8.png"),
+    BgImage: require("./bg_image.png"),
+
+
+
+    // Add more images here, matching the file names (without extension)
+}
